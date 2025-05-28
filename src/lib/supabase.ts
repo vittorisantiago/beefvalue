690037cbdf1fc9e2029db9f294dabc65a16443e4
Santiago Vittori <vittori513@gmail.com>
2025-05-28 19:38:54 -0300

    Cambios para recuperar contraseña

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true, // Habilita la detección automática del token en la URL
+  },
+});
